db81e69f2793fe8f3de5bb14f988c81fef2e7606
Sitandedi <f.nyamsi@gmail.com>
2025-08-22 04:07:13 -0400

    message

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -252,7 +256,16 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem.product);
+        setAddedToCart((prevState) => ({
+            ...prevState, [product.name]: true
+        }));
+    }
+
     return (
+        
         <div>
             <div className="navbar" style={styleObj}>
                 <div className="tag">
@@ -274,8 +287,32 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    {
+                        plantsArray.map((item, index) =>(
+                            <>
+                                <div key={index}>
+                                    <h1>{item.category}</h1>      {/* Display the category name */}
+                                </div>
+                                <div className='product-list'>
+                                    {
+                                        item.plants.map((plant, plantIndex) => (
+                                            <>
+                                                <div className='product-card' key={plantIndex}>
+                                                    <img className='product-image' src={plant.image} alt={plant.name} />    //dispay the plant image or alt text
+                                                    <div className='product-title'>{plant.name}</div>         //dispay the plant name
+                                                    <div className='product-description'>{plant.description}</div>
+                                                    <div className='product-cost'>${plant.cost}</div>
+                                                    <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>    // Handle adding plant to cart
+                                                    
+                                                </div>
+                                            </>
+                                        ))    
+                                        
+                                    }
+                                </div>
+                            </>
+                        ) )
+                    }
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
