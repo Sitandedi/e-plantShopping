b237ba8e18cd37e742c9396383b785b56f9399e2
Sitandedi <f.nyamsi@gmail.com>
2025-08-27 13:24:29 -0400

    Message

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
  const store = configureStore({
     reducer: {
+        // 'cart' is the name of the slice in the store, and it's managed by cartReducer
         cart: cartReducer,
     },
 });
-export default store
+export default store  // Export the store for use in the app (e.g., in <Provider store={store}>)
